e90c1cdbf4ea6e649298009dd063516dd2abb09d
Vitta Shradha Kumar Reddy <sv4119@srmist.edu.in>
2026-06-16 21:53:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (77.91%) - LeetSync

---
+++ 50-powx-n/powx-n.c
@@ -0,0 +1,20 @@
+double myPow(double x, int n) {
+    long long exp = n;   // handle INT_MIN safely
+
+    if (exp < 0) {
+        x = 1.0 / x;
+        exp = -exp;
+    }
+
+    double result = 1.0;
+
+    while (exp > 0) {
+        if (exp % 2 == 1)
+            result *= x;
+
+        x *= x;
+        exp /= 2;
+    }
+
+    return result;
+}
